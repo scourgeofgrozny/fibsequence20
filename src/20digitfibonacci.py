01dfd16aef7502ccc5c6b5429cfecf3f35bdd436
scourgeofgrozny <deadbydawntillyouseethesun@protonmail.com>
2022-07-30 15:07:27 +0900

    Added user input filtering; now throws ValueError if input is not type int. Implented f-string formatting as well in order to make code cleaner. Currently working on a way to make this file independent from sequence.py in that this file will import the math module and calculate Fibanocci numbers using the perfect square property. Like this, not only will there be one file less, but the digit range of the user input will be theoretically limitless (in practice, my computer would explode lol).

--- src/20digitfibonacci.py
+++ src/20digitfibonacci.py
@@ -1,22 +1,31 @@
 import time
 from sequence import fiblist
-num=int(input("Enter a number from 1-20 digits "))
-count=0 
-while count!=0:
-    break
+
+while True:
+    try:
+        num=int(input("Enter a number from 1-20 digits "))
+        if not (1<=num<=20):
+            raise ValueError()
+        break
+    except ValueError:
+        print("Please enter a valid number")
+
 if num in fiblist:
-    print("Your number is part of the Fibanocci sequence","\n","Your number: ",num)
+    print("Your number is part of the Fibanocci sequence","\n",
+    f"Your number: {num}")
     time.sleep(5)
-    count+1
+    
 elif num>10**20:
-    print("Number is too large (not between 1 and 10^20)","\n","Your number: ",num)
+    print("Number is too large (not between 1 and 10^20)","\n",
+    f"Your number: {num}")
     time.sleep(5)
-    count+1
+    
 elif num<1:
-    print("Enter a positive number lol","\n","Your number: ",num)
+    print("Enter a positive number lol","\n",
+    f"Your number: {num}")
     time.sleep(5)
-    count+1
+    
 if num not in fiblist:
-    print("Your number is not part of the Fibanocci sequence","\n","Your number: ",num)
+    print("Your number is not part of the Fibanocci sequence","\n",
+    f"Your number: {num}")
     time.sleep(5)
-    count+1
